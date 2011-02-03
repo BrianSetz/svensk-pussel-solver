afe8cffe175db7e0476df1bbc3d4b82cd45e750a
ik@briansetz.nl <ik@briansetz.nl@5cce4916-d3e1-2ee4-e3a7-b50671d45c8c>
2011-02-03 21:57:40 +0000

    Left + right clicking

--- trunk/src/nl/svenskpusselsolver/gui/PuzzleFrame.java
+++ trunk/src/nl/svenskpusselsolver/gui/PuzzleFrame.java
@@ -6,27 +6,41 @@ import java.awt.GridLayout;
 
 import javax.swing.JFrame;
 
+/**
+ * This is the main frame of the puzzle GUI.
+ */
 public class PuzzleFrame extends JFrame {
 	private Container contentPane;
 
+	/**
+	 * Puzzle frame contains all the boxes.
+	 */
 	public PuzzleFrame() {
 		contentPane = this.getContentPane();
 
-		this.initializePuzzle(10, 20);
-		
+		this.initializePuzzle(10, 10);
+
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("Svensk Pussel Solver");
 		this.pack();
 		this.setVisible(true);
 	}
 
+	/**
+	 * Initialize puzzle grid
+	 * 
+	 * @param x
+	 *            Boxes on x-axis
+	 * @param y
+	 *            Boxes on y-axis
+	 */
 	private void initializePuzzle(int x, int y) {
 		contentPane.setBackground(Color.black);
 		contentPane.setLayout(new GridLayout(y, x, 1, 1));
 
 		for (int i = 0; i < y; i++) {
 			for (int j = 0; j < x; j++) {
-				contentPane.add(new BoxPanel());
+				contentPane.add(new BoxPanel()); // New box
 			}
 		}
 	}
