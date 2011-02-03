e980b6636fa58768d737ace8532eac2d760068b3
ik@briansetz.nl <ik@briansetz.nl@5cce4916-d3e1-2ee4-e3a7-b50671d45c8c>
2011-02-03 22:06:00 +0000

    Removed SimpleGUI, updated PuzzleFrame with menu bar

--- trunk/src/nl/svenskpusselsolver/gui/PuzzleFrame.java
+++ trunk/src/nl/svenskpusselsolver/gui/PuzzleFrame.java
@@ -3,8 +3,13 @@ package nl.svenskpusselsolver.gui;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 /**
  * This is the main frame of the puzzle GUI.
@@ -19,6 +24,26 @@ public class PuzzleFrame extends JFrame {
 		contentPane = this.getContentPane();
 
 		this.initializePuzzle(10, 10);
+
+		// Build MenuBar
+		JMenuBar bar = new JMenuBar();
+		this.setJMenuBar(bar);
+		
+		// Add File menu
+		JMenu file = new JMenu("File");
+		file.setMnemonic('F');		
+		bar.add(file);
+		
+		// Add Exit item
+		JMenuItem exitItem = new JMenuItem("Exit");
+		exitItem.setMnemonic('x');
+		exitItem.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+		});
+		file.add(exitItem);				
 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("Svensk Pussel Solver");
