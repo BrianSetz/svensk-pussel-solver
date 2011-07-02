987564f51b7364499c21f468d0562f2471607134
ik@briansetz.nl <ik@briansetz.nl@5cce4916-d3e1-2ee4-e3a7-b50671d45c8c>
2011-07-02 12:41:30 +0000

    Refactored box cycling

--- trunk/src/main/java/nl/svenskpusselsolver/gui/BoxPanelMouseListener.java
+++ trunk/src/main/java/nl/svenskpusselsolver/gui/BoxPanelMouseListener.java
@@ -8,7 +8,6 @@ import javax.swing.JOptionPane;
 
 import nl.svenskpusselsolver.dataobjects.Box;
 import nl.svenskpusselsolver.dataobjects.LetterBox;
-import nl.svenskpusselsolver.dataobjects.StaticBox;
 import nl.svenskpusselsolver.dataobjects.WordBox;
 
 import org.apache.log4j.Logger;
@@ -26,57 +25,49 @@ public class BoxPanelMouseListener implements MouseListener {
 	}
 
 	@Override
-	public void mouseClicked(MouseEvent e) {
+	public void mouseClicked(MouseEvent e) {		
 		Box box = boxPanel.getBox();
 		
 		logger.trace("Button clicked (" + box.getXCoordinate() + "," + box.getYCoordinate() + ").");
-		
-		// If other button than right button is pressed or if we're clicking
-		// on a non-editable field, do nothing.
-		if ((e.getModifiers() & InputEvent.BUTTON3_MASK) != InputEvent.BUTTON3_MASK
-				|| box instanceof StaticBox)
-			return;
 
-		logger.trace("Right button clicked (" + box.getXCoordinate() + "," + box.getYCoordinate() + ").");			
-		logger.trace("Showing dialog (" + box.getXCoordinate() + "," + box.getYCoordinate() + ").");
-					
-		// Determine message
-		String message = "";
-		String currentValue = "";
-		if (box instanceof LetterBox) {
-			currentValue = String.valueOf(((LetterBox)box).getLetter());
-			message = "Enter a letter ";
-		} else {
-			currentValue = (((WordBox)box).getWord());
-			message = "Enter a word ";
-		}
-
-		// Get word or letter
-		String result = (String) JOptionPane.showInputDialog(boxPanel,
-				message, message, JOptionPane.PLAIN_MESSAGE, null, null,
-				currentValue);
-
-		logger.debug("Result from dialog: " + result + " (" + box.getXCoordinate() + "," + box.getYCoordinate() + ").");
-		
-		// Cancel button
-		if(result == null)
-			return;
-		
-		// Empty result, replace with space
-		if(result.length() <= 0)
-			result = " ";
+		// Determine which mouse button is pressed
+		if ((e.getModifiers() & InputEvent.BUTTON1_MASK) == InputEvent.BUTTON1_MASK) { // Left click
+			logger.trace("Left button clicked (" + box.getXCoordinate() + "," + box.getYCoordinate() + ").");
 			
-		
-		// Format letter				
-		if (box instanceof LetterBox) {
-			result = String.valueOf(result.charAt(0)).toUpperCase();
-			boxPanel.setBox(new LetterBox(box.getXCoordinate(), box.getYCoordinate(), result.charAt(0)));
-		} else if(box instanceof WordBox) {
-			WordBox wordBox = (WordBox) box;
-			boxPanel.setBox(new WordBox(box.getXCoordinate(), box.getYCoordinate(), result, wordBox.getDirection()));
-		} else {
-			return;
-		}
+			// Cycle box type
+			boxPanel.cycleType();
+		} else if ((e.getModifiers() & InputEvent.BUTTON2_MASK) == InputEvent.BUTTON2_MASK) { // Middle click
+			logger.trace("Middle button clicked (" + box.getXCoordinate() + "," + box.getYCoordinate() + ").");
+		} else if ((e.getModifiers() & InputEvent.BUTTON3_MASK) == InputEvent.BUTTON3_MASK) { // Right click
+			logger.trace("Right button clicked (" + box.getXCoordinate() + "," + box.getYCoordinate() + ")."); 
+						
+			// Determine message
+			String message = "";
+			String currentValue = boxPanel.getBoxValue();
+			if (box instanceof LetterBox) {
+				message = "Enter a letter ";
+			} else if(box instanceof WordBox) {
+				message = "Enter a word ";
+			} else {
+				return;
+			}
+			
+			logger.trace("Showing dialog (" + box.getXCoordinate() + "," + box.getYCoordinate() + ").");
+			
+			// Get word or letter
+			String result = (String) JOptionPane.showInputDialog(boxPanel,
+					message, message, JOptionPane.PLAIN_MESSAGE, null, null,
+					currentValue);
+			
+			logger.debug("Result from dialog: " + result + " (" + box.getXCoordinate() + "," + box.getYCoordinate() + ").");
+			
+			// Cancel button
+			if(result == null)
+				return;
+			
+			// Set result
+			boxPanel.setBoxValue(result);
+		} 
 	}
 
 	@Override
@@ -89,41 +80,6 @@ public class BoxPanelMouseListener implements MouseListener {
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		Box box = boxPanel.getBox();
-		
-		logger.trace("Button pressed (" + box.getXCoordinate() + "," + box.getYCoordinate() + ").");
-		
-		// If other button than left button is pressed, do nothing.
-		if ((e.getModifiers() & InputEvent.BUTTON1_MASK) != InputEvent.BUTTON1_MASK)
-			return;
-		
-		logger.trace("Left button pressed (" + box.getXCoordinate() + "," + box.getYCoordinate() + ").");			
-		logger.trace("Cycling box type (" + box.getXCoordinate() + "," + box.getYCoordinate() + ").");
-		
-		// Cycle box type and update box to new type
-		if(box instanceof StaticBox) {
-			boxPanel.updateType(new WordBox(box.getXCoordinate(), box.getYCoordinate(), "", WordBox.Direction.UP));
-		} else if(box instanceof WordBox) {
-			WordBox wordBox = (WordBox) box;
-			WordBox.Direction direction = wordBox.getDirection();
-			
-			switch (direction) {
-				case UP:
-					boxPanel.updateType(new WordBox(box.getXCoordinate(), box.getYCoordinate(), "", WordBox.Direction.RIGHT));
-					break;
-				case RIGHT:
-					boxPanel.updateType(new WordBox(box.getXCoordinate(), box.getYCoordinate(), "", WordBox.Direction.DOWN));
-					break;
-				case DOWN:
-					boxPanel.updateType(new WordBox(box.getXCoordinate(), box.getYCoordinate(), "", WordBox.Direction.LEFT));
-					break;
-				case LEFT:
-					boxPanel.updateType(new LetterBox(box.getXCoordinate(), box.getYCoordinate()));
-					break;
-			}
-		} else if(box instanceof LetterBox) {
-			boxPanel.updateType(new StaticBox(box.getXCoordinate(), box.getYCoordinate()));
-		}
 	}
 
 	@Override
